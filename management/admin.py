cd5f16ec5e18e4c4d5f1a6a74aa06f0fc7260749
Anik Islam <68261265+AnikIslam1@users.noreply.github.com>
2022-12-02 20:08:12 +0600

    model update

--- management/admin.py
+++ management/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Company
 from .models import Employee
-
+from .models import Device
+#company account registration
 class CompanyAccount(admin.ModelAdmin):
     list_display = ('pk','company_name','company_status')
 admin.site.register(Company, CompanyAccount)
-
+#employee account registration
 class EmployeeAccount(admin.ModelAdmin):
     list_display = ('pk','employee_name')
 admin.site.register(Employee, EmployeeAccount)
+#device status registration
+class DeviceStatus(admin.ModelAdmin):
+    list_display = ('pk','device_name','device_status','device_given_date','device_received_date')
+admin.site.register(Device, DeviceStatus)
--- management/models.py
+++ management/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+
 #importing auth.models for company andim user.
 from django.contrib.auth.models import User
 #Company model creation using auth.models usre or company admin assign from superadmin. 
 class Company(models.Model):
-  company_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
   company_name = models.CharField(max_length=50)
   company_status = ('Active')
   def __str__(self):
     return self.company_name
 #Employee model creation using Company models foreignKey
 class Employee(models.Model):
-    employee_name = models.ForeignKey(Company, on_delete=models.CASCADE)
+    
+    employee_name = models.CharField(max_length=50)
     def __str__(self):
         return self.employee_name   
+
+#Products or devices model creation
+class Device(models.Model):
+    device_name = models.CharField(max_length=50)
+    device_status = models.CharField(max_length=50)
+    device_given_date = models.DateField()
+    device_received_date = models.DateField()
+    def __str__(self):
+        return self.Device_name   
